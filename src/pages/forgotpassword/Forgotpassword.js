a4c2748a1ca5b0d625444c5e3a2515425b78d408
Alamin-Coding <alamin.codelover@gmail.com>
2022-11-11 23:15:30 +0600

    password update added

--- src/pages/forgotpassword/Forgotpassword.js
+++ src/pages/forgotpassword/Forgotpassword.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 const Forgotpassword = () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleUpdatePassword = () => {
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            navigate("/login")
+            alert("Password reset email sent!")
+        })
+        .catch((error) => {
+            alert(error.code)
+
+        });
+    }
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
         <div className='w-2/4 h-2/4 bg-white rounded-md p-5 drop-shadow-md'>
@@ -9,10 +32,10 @@ const Forgotpassword = () => {
                 <h2 className='text-4xl pb-4 text-slate-700 font-poppins font-medium '>Update Password</h2>
                 <hr />
                 <div className='pt-4'>
-                    <input className='py-5 px-4 rounded border border-borderColor w-full' type="text" placeholder='First Name' />
+                    <input className='py-5 px-4 rounded border border-borderColor w-full' type="text" placeholder='Email' onChange={handleEmail} />
                 </div>
                 <div className='flex gap-3 mt-4'>
-                    <button className='w-72 bg-primary py-5 text-white text-center rounded-md text-xl font-poppins font-bold'>Update password</button>
+                    <button className='w-72 bg-primary py-5 text-white text-center rounded-md text-xl font-poppins font-bold' onClick={handleUpdatePassword}>Update password</button>
                     <Link to="/login" className='w-72 bg-orange-600 py-5 text-white text-center rounded-md text-xl font-poppins font-bold'>Back</Link>
                 </div>
             </div>
